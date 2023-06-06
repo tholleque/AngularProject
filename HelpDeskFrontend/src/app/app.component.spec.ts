db71592572db1c3f11472cf027390f7cc5cad279
Tim Holleque <timothy.t.holleque@gmail.com>
2023-06-06 15:49:19 -0400

    idk

--- HelpDeskFrontend/src/app/app.component.spec.ts
+++ HelpDeskFrontend/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'HelpDeskBackend'`, () => {
+  it(`should have as title 'HelpDeskFrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('HelpDeskBackend');
+    expect(app.title).toEqual('HelpDeskFrontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('HelpDeskBackend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('HelpDeskFrontend app is running!');
   });
 });
